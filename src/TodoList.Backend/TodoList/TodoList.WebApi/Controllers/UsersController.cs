41c0c54ec7ea6491ffef33dc87fb4b4748ca2d4d
Ivan <zhmur.ivan@gmail.com>
2024-08-25 19:28:24 +0300

    chore(backend): remove test migration

--- src/TodoList.Backend/TodoList/TodoList.WebApi/Controllers/UsersController.cs
+++ src/TodoList.Backend/TodoList/TodoList.WebApi/Controllers/UsersController.cs
@@ -1,30 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TodoList.Application.Interfaces;
 
 namespace TodoList.WebApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("[controller]")]
 public class UsersController : ControllerBase
 {
-    private readonly IDbContext _db;
-
-    public UsersController(IDbContext db)
-    {
-        _db = db;
-    }
-
-    [HttpGet("test")]
-    public async Task<IActionResult> Test()
-    {
-        var users = await _db.Users.AsNoTracking().ToListAsync();
-
-        if (users is null)
-        {
-            return Ok("Empty");
-        }
-
-        return Ok(users);
-    }
+    
 }
