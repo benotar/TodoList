20b93fe3650fecebecc31fcdb09ca3e3dd38ff05
Ivan <zhmur.ivan@gmail.com>
2024-10-21 11:43:25 +0300

    fix(frontend): reinstall shadcn ui and tailwind css to fix theme switching issue

--- src/TodoList.Frontend/vite.config.ts
+++ src/TodoList.Frontend/vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'path'
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    watch: {
-      usePolling: true
-    },
-    strictPort: true
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    strictPort: true
+  }
+});
 
 
