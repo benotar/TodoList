edc4f28c9963df1989d4299147bd553509894b1f
Ivan <zhmur.ivan@gmail.com>
2024-11-15 11:47:23 +0200

    feat(frontend): add advanced/basic home page

--- src/TodoList.Frontend/src/pages/home/AdvancedHomePage.tsx
+++ src/TodoList.Frontend/src/pages/home/AdvancedHomePage.tsx
@@ -1,7 +1,19 @@
 import {FC} from "react";
+import LinkButton from "@/components/shared/LinkButton.tsx";
+import HomeCardWrapper from "@/components/home/HomeCardWrapper.tsx";
 
 const AdvancedHomePage: FC =  () => {
-    return (<p>Hello Advanced</p>);
+    return (
+        <HomeCardWrapper
+            title="Admin Dashboard"
+            description="Manage users and todos. Keep track of all activities and maintain a well-organized todo environment for your team."
+        >
+            <div className="flex flex-col justify-center items-center gap-y-5">
+                <LinkButton label={"View Admin Management"} link={"/admin"}/>
+                <LinkButton label={"Manage todos"} link={"/todo"}/>
+            </div>
+        </HomeCardWrapper>
+    );
 };
 
 export default AdvancedHomePage
--- src/TodoList.Frontend/src/pages/home/BasicHomePage.tsx
+++ src/TodoList.Frontend/src/pages/home/BasicHomePage.tsx
@@ -1,7 +1,18 @@
 import {FC} from "react";
+import LinkButton from "@/components/shared/LinkButton.tsx";
+import HomeCardWrapper from "@/components/home/HomeCardWrapper.tsx";
 
 const BasicHomePage: FC =  () => {
-    return (<p>Hello Basic</p>);
+    return (
+        <HomeCardWrapper
+            title="Basic Dashboard"
+            description="Manage your todos. Keep track of all activities and maintain a well-organized todo environment for your team."
+        >
+            <div className="flex flex-col justify-center items-center gap-y-5">
+                <LinkButton label={"Manage todos"} link={"/todo"}/>
+            </div>
+        </HomeCardWrapper>
+    );
 };
 
 export default BasicHomePage
